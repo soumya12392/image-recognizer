01c80f80eed41ec2c8e457bae5e8253617afd224
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2021-11-10 18:56:51 +0530

    Add files via upload
    Updated "code.js"

--- code.js
+++ code.js
@@ -17,8 +17,25 @@ function take_snapshot() {
 
 console.log("ml5 version: " + ml5.version);
 
-var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/X1zTnr2DZ/model.json", modelLoaded());
+var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/X1zTnr2DZ/model.json", modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded!");
+}
+
+function check() {
+    var img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
